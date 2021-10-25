ca4864164eeaa77674514f67043855e6ebaa31de
Ravindra Verma <rverma@cmslpc125.fnal.gov>
2021-10-24 23:03:13 -0500

    adding variabls in automated way

---
+++ MVA_Ntuple/Disc_Ntuple/VarInfo.py
@@ -0,0 +1,58 @@
+
+def GetVarInfo():
+    varDict = { 
+             "Muon_pt"              : ["Muon_pt"     , [30,0,1650], True],
+             "Muon_eta"             : ["Muon_eta"    , [12,-2.88,2.88], True],
+             "Muon_phi"             : ["Muon_phi"    , [20,-5.0,5.0], True],
+             "Electron_pt"          : ["Electron_pt"    , [30,0,1500], True],
+             "Electron_eta_sc"      : ["Electron_eta_sc" , [12,-2.88,2.88], True],
+             "Electron_phi"         : ["Electron_phi"   , [20,-5.0,5.0], True],
+             "Photon_et"             : ["Photon_et"    , [100,0,2000], True],
+             "Photon_size"          : ["Photon_size"     , [10,-0.5,9.5], True],
+             "Jet_size"             : ["Jet_size"     , [16,-0.5,15.5], True],
+             "Jet_b_size"           : ["Jet_b_size"     , [10,-0.5,9.5], True],
+             "Jet_pt"               : ["Jet_pt"    , [30,-20,1480], True],
+             "Jet_eta"             : ["Jet_eta"    , [12,-2.88,2.88], True],
+             "Reco_met"             : ["Reco_met"      , [30,-20,1180], True],
+             "Reco_mass_lgamma"     : ["Reco_mass_lgamma", [30,0,300], True],
+             "Reco_mass_t_had"      : ["Reco_mass_t_had", [30,0,3000], True],
+             "Reco_mass_t_lep"      : ["Reco_mass_t_lep", [30,0,3000], True],
+             "Reco_ht"              : ["Reco_ht"        , [30,0,6000], True],
+             "Reco_st"              : ["Reco_st"        , [30,0,6000], True],
+             "Reco_mass_T_had"      : ["Reco_mass_T_had", [30,0,6000], True],
+             "Reco_mass_T_lep"      : ["Reco_mass_T_lep", [30,0,6000], True],
+             "Reco_mass_T"          : ["Reco_mass_T", [30,0,6000], True],
+             "Reco_mass_TT"         : ["Reco_mass_TT", [30,0,6000], True],
+             "Reco_chi2"            : ["Reco_chi2", [30, 0,1200], True],
+             "FatJet_size"             : ["FatJet_size"            , [5,-0.5,4.5], True],
+             "FatJet_pt"               : ["FatJet_pt"              , [30,0,3000], True],
+             "FatJet_eta"              : ["FatJet_eta"             , [12,-2.88,2.88], True],
+             "FatJet_mass"             : ["FatJet_mass"            , [30,0,1200], True],
+             "FatJet_msoftdrop"        : ["FatJet_msoftdrop"       , [30,0,1200], True],
+             "FatJet_btagDeepB"        : ["FatJet_btagDeepB"       , [20,-0.5,1.5], True],
+             "FatJet_deepTagMD_TvsQCD" : ["FatJet_deepTagMD_TvsQCD", [20,-0.5,1.5], True],
+             "FatJet_deepTagMD_WvsQCD" : ["FatJet_deepTagMD_WvsQCD", [20,-0.5,1.5], True],
+             "FatJet_deepTag_TvsQCD"   : ["FatJet_deepTag_TvsQCD"  , [20,-0.5,1.5], True],
+             "FatJet_deepTag_WvsQCD"   : ["FatJet_deepTag_WvsQCD"  , [20,-0.5,1.5], True],
+             }
+    varDictTemp = {
+             #"Muon_pt"              : ["Muon_pt"     , [30,0,1650], True],
+             #"Muon_eta"             : ["Muon_eta"    , [12,-2.88,2.88], True],
+             #"Electron_pt"          : ["Electron_pt"    , [30,0,1500], True],
+             #"Electron_eta_sc"      : ["Electron_eta_sc" , [12,-2.88,2.88], True],
+             ##"Jet_pt"               : ["Jet_pt"    , [30,-20,1480], True],
+             #"Jet_eta"             : ["Jet_eta"    , [12,-2.88,2.88], True],
+             #"Reco_met"             : ["Reco_met"      , [30,-20,1180], True],
+             #"Reco_mass_lgamma"     : ["Reco_mass_lgamma", [30,0,300], True],
+             #"Jet_size"             : ["Jet_size"     , [16,-0.5,15.5], True],
+             #"Jet_b_size"           : ["Jet_b_size"     , [10,-0.5,9.5], True],
+             "Reco_ht"              : ["Reco_ht"        , [300,0,9000], True],
+             "Reco_st"              : ["Reco_st"        , [300,0,9000], True],
+             #"Photon_et"             : ["Photon_et"    , [100,0,2000], True],
+             "Reco_mass_T"          : ["Reco_mass_T", [300,0,6000], True]
+             }
+    return varDictTemp
+    #return hDict 
+    #return hDictWeight 
+
+allVarList = GetVarInfo().keys()
